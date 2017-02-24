c43d872d09d58466a3d561d08c7a0bc5514edfd4
Andre Phu-Van Nguyen <andre-phu-van.nguyen@polymtl.ca>
2017-02-24 15:37:30 -0500

    eq constraints

---
+++ projet/matlab/buildEqualityConstraints.m
@@ -0,0 +1,27 @@
+function [ Aeq, beq ] = buildEqualityConstraints( n, m, k_r, k_psi, t, w)
+%BUILDEQUALITYCONSTRAINTS Builds the the equality constraint matrices to be
+%fed to quadprog
+% Inputs:
+%   n           Order of the polynomials of a trajectory
+%   m           Number of waypoints
+%   k_r         Order of the derivative of the position
+%   k_psi       Order of the derivative of the yaw angle
+%   t           Vector of the arrival times for each waypoint. Should
+%               always 0 as the first element.
+%   w           Waypoints, in the paper they call them keyframes. Each row
+%               is a waypoint with the data in this order: x y z psi vx vy
+%               vz vpsi ax ay az apsi
+
+% Author:   Andre Phu-Van Nguyen <andre-phu-van.nguyen@polymtl.ca>
+
+% Mathematical basis
+% Recall the form of the quadratic optimization problem to be
+%   min c'Hc + f'c
+% Where c is an 4mn x 1 vector containing the coefficients of all the
+% polynomials describing each dimention of the trajectory. In this function
+% we are building the equality matrix constraints where
+%   Aeq * x = beq 
+% Where Aeq is a matrix of doubles and beq is a vector of doubles
+
+end
+
---
+++ projet/matlab/getexponents.m
@@ -0,0 +1,7 @@
+function [ output_args ] = getexponents( polynomial_order,  )
+%GETEXPONENTS Summary of this function goes here
+%   Detailed explanation goes here
+
+
+end
+
