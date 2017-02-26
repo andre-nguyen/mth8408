a8426b2dc3ef4dadc004aa04e773452747fdc509
Andre Nguyen <andre-phu-van.nguyen@polymtl.ca>
2017-02-26 02:30:58 -0500

    update

--- exercices/matlab/model_problems.m
+++ exercices/matlab/model_problems.m
@@ -4,10 +4,22 @@
 % Auteur: Andre Phu-Van Nguyen <andre-phu-van.nguyen@polymtl.ca>
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 clc;
-clear;
 close all;
 
-[X1,X2] = meshgrid(-20:2:20);
+%% problem 2
+[X1,X2] = meshgrid(-2:0.1:2);
 Z = 100 .* (X2 - X1.^2).^2 + (1 - X1).^2;
+
 figure
+subplot(1, 2, 1)
 surf(X1, X2, Z);
+xlabel('x1')
+ylabel('x2')
+zlabel('f(x1, x2)')
+title('Problem 2')
+
+subplot(1, 2, 2)
+contour(X1, X2, Z, 10)
+xlabel('x1')
+ylabel('x2')
+title('Contour plot')
